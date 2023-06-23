aeccf0354725221e7f59535f31fc50cfe3b971fd
Jay Faust <jay.faust3@gmail.com>
2023-06-22 23:21:08 -0400

    Got replica set working with custom MongoClientFactoryBean

---
+++ src/main/kotlin/com/configuration/MongoDBConfiguration.kt
@@ -0,0 +1,24 @@
+package com.saint.api.configuration
+
+import java.util.function.Consumer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.data.mongodb.core.MongoClientFactoryBean
+import com.mongodb.ConnectionString
+
+@Component
+class MongoDBConfiguration {
+    @Bean
+    fun mongo(
+        @Value("\${mongodb.uri}") uri: String,
+        @Value("\${mongodb.replicaSet}") replicaSet: String
+    ): MongoClientFactoryBean {
+        val mongo = MongoClientFactoryBean()
+        mongo.setConnectionString(
+            ConnectionString(uri)
+        )
+        mongo.setReplicaSet(replicaSet)
+        return mongo
+    }
+}
