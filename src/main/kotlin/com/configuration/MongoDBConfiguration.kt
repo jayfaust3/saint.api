af58eff55d25a6c905e5cd2fa7392583f83c1235
Jay Faust <jay.faust3@gmail.com>
2023-06-24 00:05:08 -0400

    Updated mongo config to support change streams

--- src/main/kotlin/com/configuration/MongoDBConfiguration.kt
+++ src/main/kotlin/com/configuration/MongoDBConfiguration.kt
@@ -2,22 +2,41 @@ package com.saint.api.configuration
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
-import org.springframework.data.mongodb.core.MongoClientFactoryBean
 import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
-@Component
-class MongoDBConfiguration {
+@Configuration
+@EnableMongoRepositories(basePackages = ["com.saint.api.dataaccess.repositories"])
+class MongoConfig(@Value("\${mongodb.uri}")
+                  private val uri: String,
+                  @Value("\${mongodb.saintDBName}")
+                  private val saintDBName: String,
+                  @Value("\${mongodb.replicaSet}")
+                  private val replicaSet: String) : AbstractMongoClientConfiguration() {
+
+                      override fun getDatabaseName(): String {
+        return saintDBName
+    }
+
     @Bean
-    fun mongo(
-        @Value("\${mongodb.uri}") uri: String,
-        @Value("\${mongodb.replicaSet}") replicaSet: String
-    ): MongoClientFactoryBean {
-        val mongo = MongoClientFactoryBean()
-        mongo.setConnectionString(
-            ConnectionString(uri)
-        )
-        mongo.setReplicaSet(replicaSet)
-        return mongo
+    override fun mongoClient(): MongoClient {
+        val connectionString = ConnectionString("$uri/$saintDBName")
+
+        val settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build()
+
+        return MongoClients.create(settings)
+    }
+
+    @Bean
+    fun mongoTemplate(): MongoTemplate {
+        return MongoTemplate(mongoClient(), databaseName)
     }
 }
